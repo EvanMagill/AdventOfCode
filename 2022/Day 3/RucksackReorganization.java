a80e3e3e76a28f7ac65fa0396558ac2180b94706
Evan Magill <magillep@gmail.com>
2022-12-15 22:15:34 -0500

    Add files via upload

--- 2022/Day 3/RucksackReorganization.java
+++ 2022/Day 3/RucksackReorganization.java
@@ -8,33 +8,33 @@ public class RucksackReorganization {
 		File rucksackFile = new File("src/rucksacks.txt");
 		Scanner console = new Scanner(rucksackFile);
 		long prioritySum = 0;
-		while(console.hasNextLine()) {
-            String curLine = console.nextLine();
-            int halfWay = curLine.length()/2;
-            long[] halves = new long[2];
-            int start = 0;
-            int stop = halfWay;
-            for(int half = 0; half < 2; half ++) {
-                for(int i = start; i < stop; i ++) {
-                    char curChar = curLine.charAt(i);
-                    int curPriority = curChar - 'a';
-                    if(curPriority < 0) {
-                        curPriority = curChar - 'A' + 26;
-                    }
-                    halves[half] |= (1l << curPriority);
-                }
-                start = halfWay;
-                stop = curLine.length();
-            }
-            long usedTwice = halves[0] & halves[1];
-            int priority = 0;
-            do {
-                priority ++;
-                usedTwice >>= 1;
-            }
-            while(usedTwice > 0);
-            prioritySum += priority;
-        }
+		while (console.hasNextLine()) {
+			String curLine = console.nextLine();
+			int halfWay = curLine.length() / 2;
+			long[] halves = new long[2];
+			int start = 0;
+			int stop = halfWay;
+			for (int half = 0; half < 2; half++) {
+				for (int i = start; i < stop; i++) {
+					char curChar = curLine.charAt(i);
+					int curPriority = curChar - 'a';
+					if (curPriority < 0) {
+						curPriority = curChar - 'A' + 26;
+					}
+					halves[half] |= (1l << curPriority);
+				}
+				start = halfWay;
+				stop = curLine.length();
+			}
+			long usedTwice = halves[0] & halves[1];
+			int priority = 0;
+			do {
+				priority++;
+				usedTwice >>= 1;
+			} while (usedTwice > 0);
+			prioritySum += priority;
+		}
+		console.close();
 		System.out.println(prioritySum);
 	}
 
--- 2022/Day 3/RucksackReorganizationStar2.java
+++ 2022/Day 3/RucksackReorganizationStar2.java
@@ -8,29 +8,29 @@ public class RucksackReorganizationStar2 {
 		File rucksackFile = new File("src/rucksacks.txt");
 		Scanner console = new Scanner(rucksackFile);
 		long prioritySum = 0;
-		while(console.hasNextLine()) {
+		while (console.hasNextLine()) {
 			long[] elves = new long[3];
-			
-			for(int elf = 0; elf <3; elf ++) {
+
+			for (int elf = 0; elf < 3; elf++) {
 				String curLine = console.nextLine();
-				for(int i = 0; i < curLine.length(); i ++) {
-                    char curChar = curLine.charAt(i);
-                    int curPriority = curChar - 'a';
-                    if(curPriority < 0) {
-                        curPriority = curChar - 'A' + 26;
-                    }
-                    elves[elf] |= (1l << curPriority);
-                }
+				for (int i = 0; i < curLine.length(); i++) {
+					char curChar = curLine.charAt(i);
+					int curPriority = curChar - 'a';
+					if (curPriority < 0) {
+						curPriority = curChar - 'A' + 26;
+					}
+					elves[elf] |= (1l << curPriority);
+				}
 			}
 			long usedThrice = elves[0] & elves[1] & elves[2];
-            int priority = 0;
-            do {
-                priority ++;
-                usedThrice >>= 1;
-            }
-            while(usedThrice > 0);
-            prioritySum += priority;
-        }
+			int priority = 0;
+			do {
+				priority++;
+				usedThrice >>= 1;
+			} while (usedThrice > 0);
+			prioritySum += priority;
+		}
+		console.close();
 		System.out.println(prioritySum);
 	}
 
