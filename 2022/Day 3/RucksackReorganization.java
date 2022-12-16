f01b6df6461d1a66436e7cc698185b16534d954d
Evan Magill <magillep@gmail.com>
2022-12-15 21:26:07 -0500

    Add files via upload

---
+++ 2022/Day 3/RucksackReorganization.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RucksackReorganization {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File rucksackFile = new File("src/rucksacks.txt");
+		Scanner console = new Scanner(rucksackFile);
+		long prioritySum = 0;
+		while(console.hasNextLine()) {
+            String curLine = console.nextLine();
+            int halfWay = curLine.length()/2;
+            long[] halves = new long[2];
+            int start = 0;
+            int stop = halfWay;
+            for(int half = 0; half < 2; half ++) {
+                for(int i = start; i < stop; i ++) {
+                    char curChar = curLine.charAt(i);
+                    int curPriority = curChar - 'a';
+                    if(curPriority < 0) {
+                        curPriority = curChar - 'A' + 26;
+                    }
+                    halves[half] |= (1l << curPriority);
+                }
+                start = halfWay;
+                stop = curLine.length();
+            }
+            long usedTwice = halves[0] & halves[1];
+            int priority = 0;
+            do {
+                priority ++;
+                usedTwice >>= 1;
+            }
+            while(usedTwice > 0);
+            prioritySum += priority;
+        }
+		System.out.println(prioritySum);
+	}
+
+}
---
+++ 2022/Day 3/RucksackReorganizationStar2.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RucksackReorganizationStar2 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File rucksackFile = new File("src/rucksacks.txt");
+		Scanner console = new Scanner(rucksackFile);
+		long prioritySum = 0;
+		while(console.hasNextLine()) {
+			long[] elves = new long[3];
+			
+			for(int elf = 0; elf <3; elf ++) {
+				String curLine = console.nextLine();
+				for(int i = 0; i < curLine.length(); i ++) {
+                    char curChar = curLine.charAt(i);
+                    int curPriority = curChar - 'a';
+                    if(curPriority < 0) {
+                        curPriority = curChar - 'A' + 26;
+                    }
+                    elves[elf] |= (1l << curPriority);
+                }
+			}
+			long usedThrice = elves[0] & elves[1] & elves[2];
+            int priority = 0;
+            do {
+                priority ++;
+                usedThrice >>= 1;
+            }
+            while(usedThrice > 0);
+            prioritySum += priority;
+        }
+		System.out.println(prioritySum);
+	}
+
+}
